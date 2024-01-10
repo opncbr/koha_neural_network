004424755ddf922d1b8b74eee7ac43be8b51e432
LumRamabaja <lumramabaja@gmail.com>
2024-01-10 14:49:51 +0100

    neighborhood function

--- koha/config.py
+++ koha/config.py
@@ -7,7 +7,6 @@ class KohaInputLayerConfig():
     emb_dim: int = 100 # dimensionality of vocabulary embeddings
     window_size: int = 10 # temporal window to learn the signature embeddings
     neg_sampling_num: int = 20 # number of negative samples
-    sample = 1e-3 # constant used to control positive sampling
     neg_unigram_scale = 10 # used for negative sampling
     sparse = True # used for the embeddings of the signatures
 
@@ -15,7 +14,9 @@ class KohaInputLayerConfig():
 class KohaLayerConfig():
     unit_num: int = 100 # number of units
     emb_dim: int = 100 # dimensionality of signature (output) and weights (input)
-    receptive_field = 10 # the number of layers (above & below) from which a unit receives / sends inputs/outputs to.
+    receptive_field = 10 # the number of previous time steps from which a unit receives / sends inputs / outputs to.
+    context_size : int = 50
     n_head: int = 12 # number of heads. Each unit receives inputs from each winner of each head within the attention radius
-    window_size: int = 10 # temporal window to learn the signature embeddings
     neg_sampling_num: int = 20 # number of negative samples
+    neg_unigram_scale: int = 10
+    sparse = True # used for the embeddings of the signatures
--- koha/koha_layer.py
+++ koha/koha_layer.py
@@ -1,6 +1,7 @@
 import torch
 from torch.nn.parameter import Parameter
 from .config import KohaLayerConfig
+from torch.nn import Embedding
 from math import sqrt
 
 class KohaLayer(torch.nn.Module):
@@ -8,9 +9,10 @@ class KohaLayer(torch.nn.Module):
         super().__init__()
         self.unit_num = config.unit_num
         self.emb_dim = config.emb_dim
-        self.window_size = config.window_size
         self.receptive_field = config.receptive_field
-        self.signatures = Parameter(torch.empty((self.unit_num, self.emb_dim)), requires_grad=True)
+        self.neg_sampling_num = config.neg_sampling_num
+        self.EPS = 1e-15
+        self.signatures = Embedding(self.unit_num, self.emb_dim, sparse= config.sparse)
         self.weights = Parameter(torch.empty((self.unit_num, self.receptive_field, self.emb_dim)), requires_grad=False)
         self.previous_winners = []
         self.reset_parameters()
@@ -20,3 +22,35 @@ class KohaLayer(torch.nn.Module):
         torch.nn.init.kaiming_uniform_(self.weights, a=sqrt(5))
 
     
+    def gaussian_neighborhood(num_neurons, sigma, inverse = False):
+        center_neuron = num_neurons // 2
+        neuron_indices = torch.arange(num_neurons)
+        distance = torch.abs(neuron_indices - center_neuron)
+
+        # smaller sigmas result in narrower neighborhood functions
+        influence = torch.exp(-distance.pow(2) / (2 * torch.tensor(sigma).pow(2)))
+        if inverse:
+            influence_prob = 1 - influence
+            influence_prob[center_neuron] = 0
+            influence_prob = influence_prob / influence_prob.sum()
+        else:
+            influence[center_neuron] = 0
+            influence_prob = influence / influence.sum()
+        return influence_prob
+    
+    def _neghborhood_function(self, winner, sigma, inverse):
+        distribution = self.gaussian_neighborhood(self.unit_num, sigma, inverse)
+        remaining = self.unit_num - winner
+        center_neuron = self.unit_num // 2
+        rearranged_distribution = torch.cat([distribution[center_neuron:], distribution[:center_neuron]])
+        return torch.cat([rearranged_distribution[- winner :], rearranged_distribution[: remaining]])
+
+    def positive_neighborhood_function(self, winner, sigma):
+        return self._neghborhood_function(winner, sigma, inverse=False)
+
+    def negative_neighborhood_function(self, winner, sigma):
+        return self._neghborhood_function(winner, sigma, inverse=True)
+    
+    
+# have a key value pair for each synapse. perform competitive learning for each synapse. take the scores of the BMUs and perform softmax, combine them, create one combines signature.
+# no hebbian update rule required. the synapse as well as the signature get udpated through gradient descent. 
