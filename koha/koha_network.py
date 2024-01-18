152981d55571d15d6e5c1bbefa56ae94e31e200e
LumRamabaja <lumramabaja@gmail.com>
2024-01-18 14:24:14 +0100

    koha_network draft

--- koha/koha_network.py
+++ koha/koha_network.py
@@ -12,6 +12,8 @@ class KohaNetwork(torch.nn.Module):
         super().__init__()
         self.vocab_size = network_config.vocab_size
         self.emb_dim = network_config.emb_dim
+        self.context = network_config.context
+        self.receptive_field = block_config.receptive_field
         self.embeddings = Embedding(self.vocab_size, self.emb_dim, sparse=True)
         self.koha_blocks = [
             KohaBlock(block_config, True)
@@ -19,7 +21,43 @@ class KohaNetwork(torch.nn.Module):
             else KohaBlock(block_config, False)
             for ind in range(network_config.context)
         ]
+        self.network_state = None
+        self.unfold = torch.nn.Unfold(
+            kernel_size=(self.emb_dim, self.receptive_field),
+            dilation=1,
+            padding=0,
+            stride=1,
+        )
         self.reset_parameters()
 
     def reset_parameters(self):
         torch.nn.init.kaiming_uniform_(self.embeddings.weight, a=sqrt(5))
+
+    def initialize_state(self, batch=1):
+        self.network_state = torch.zeros(
+            batch, self.emb_dim, self.context + self.receptive_field - 1
+        )
+
+    def forward(self, input_indices):
+        batch = input_indices.size(0)
+        assert (
+            self.network_state is not None
+        ), "initialize_state must be called before using forward"
+        input = self.embeddings(input_indices)
+        X = torch.cat(
+            [
+                input.unsqueeze(0),
+                self.network_state.permute(2, 0, 1)[: self.context - 1, :, :],
+            ]
+        )
+        Z = (
+            self.unfold(self.network_state.unsqueeze(1))
+            .permute(2, 0, 1)
+            .view(-1, batch, self.emb_dim, self.receptive_field)
+            .transpose(-1, -2)
+        )
+        y = []
+        for block_ind, block in enumerate(self.koha_blocks):
+            x, z = X[block_ind], Z[block_ind]
+            y = block(x, z)
+            self.network_state[:, :, block_ind] = y
