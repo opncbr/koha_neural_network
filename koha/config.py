105852d2b32968f7ca4ef0cfe1d7f515a4dd5b59
LumRamabaja <lumramabaja@gmail.com>
2024-01-30 22:02:19 +0100

    removed window size

--- koha/config.py
+++ koha/config.py
@@ -6,7 +6,6 @@ class KohaBlockConfig:
     emb_dim: int = 16  # embedding dimension
     head_num: int = 4  # number of heads
     receptive_field = 5  # number of connections to other Koha blocks
-    window_size: int = 5  # temporal window used for positve and negative sampling
     weight_decay: float = 1e-1
     learning_rate: float = 6e-4
     beta1: float = 0.9
